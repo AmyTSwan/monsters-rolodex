0c3a032d227348b021d35e7b99019d31459b8b0a
Amy VS <amy.vaillancourtsals@thinkshout.com>
2020-02-09 18:59:49 -0800

    Updates

---
+++ precache-manifest.e1669981d727d29a4627e511a38f3605.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31444f46bdbd94164b8332a443abe0cf",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "429616b7448159cc2a90",
+    "url": "/monsters-rolodex/static/css/main.655fcf2b.chunk.css"
+  },
+  {
+    "revision": "5d89af96b37a9907cc40",
+    "url": "/monsters-rolodex/static/js/2.8b93a7b4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.8b93a7b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "429616b7448159cc2a90",
+    "url": "/monsters-rolodex/static/js/main.2cd52e1a.chunk.js"
+  },
+  {
+    "revision": "e2d28b852f4a3058c6a7",
+    "url": "/monsters-rolodex/static/js/runtime-main.d3301444.js"
+  }
+]);
